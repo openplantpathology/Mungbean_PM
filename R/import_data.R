e6c992bad61a7a7cdd40ba75100842d39f64c793
PaulMelloy <paul@melloy.com.au>
2019-09-20 15:15:33 +1000

    rewrote without the need for tidyverse corrected headers

--- R/import_data.R
+++ R/import_data.R
@@ -11,28 +11,28 @@
 #'
 import_data <- function() {
    x <-
-      read_csv("data/1902 powdery mildew-Mungbean - Collated means.csv")
-   x <- x %>%
-      mutate(trial_ref = as.factor(trial_ref)) %>%
-      mutate(location = as.factor(location)) %>%
-      mutate(host_genotype = as.factor(host_genotype)) %>%
-      mutate(fungicide_ai = as.factor(fungicide_ai)) %>%
-      mutate(trade_name = as.factor(trade_name)) %>%
-      mutate(year = as.factor(year)) %>%
-      mutate(replicates = as.factor(replicates)) %>%
-      mutate(planting_date = as.Date(planting_date, format = "%d/%m/%Y")) %>%
-      mutate(emergence_date = as.Date(emergence_date, format = "%d/%m/%Y")) %>%
-      mutate(flowering_date = as.Date(flowering_date, format = "%d/%m/%Y")) %>%
-      mutate(pod_final_date = as.Date(pod_final_date, format = "%d/%m/%Y")) %>%
-      mutate(mid_late_pod_final = as.Date(mid_late_pod_final, format = "%d/%m/%Y")) %>%
-      mutate(first_sign_disease = as.Date(first_sign_disease, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_1 = as.Date(fungicide_application_1, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_2 = as.Date(fungicide_application_2, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_3 = as.Date(fungicide_application_3, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_4 = as.Date(fungicide_application_4, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_5 = as.Date(fungicide_application_5, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_6 = as.Date(fungicide_application_6, format = "%d/%m/%Y")) %>%
-      mutate(fungicide_application_7 = as.Date(fungicide_application_7, format = "%d/%m/%Y")) %>%
-      mutate(harvest_date = as.Date(harvest_date, format = "%d/%m/%Y")) %>%
-      mutate(final_assessment = as.Date(final_assessment, format = "%d/%m/%Y"))
+      read.csv("data/1902 powdery mildew-Mungbean - Collated means.csv")
+   x$trial_ref <- as.factor(x$trial_ref)
+   x$location <- as.factor(x$location)
+   x$host_genotype <- as.factor(x$host_genotype)
+   x$fungicide_ai <- as.factor(x$fungicide)
+   x$trade_name <- as.factor(x$trade_name)
+   x$year <- as.factor(x$year)
+   x$replicates <- as.factor(x$replicates)
+   x$planting_date <- as.Date(x$planting_date, format = "%d/%m/%Y")
+   x$emergence_date <- as.Date(x$emergence_date, format = "%d/%m/%Y")
+   x$flowering_date <- as.Date(x$flowering_date, format = "%d/%m/%Y")
+   x$pod_fill_date <- as.Date(x$pod_fill_date, format = "%d/%m/%Y")
+   x$mid_late_pod_fill <- as.Date(x$mid_late_pod_fill, format = "%d/%m/%Y")
+   x$first_sign_disease <- as.Date(x$first_sign_disease, format = "%d/%m/%Y")
+   x$fungicide_application_1 <- as.Date(x$fungicide_application_1, format = "%d/%m/%Y")
+   x$fungicide_application_2 <- as.Date(x$fungicide_application_2, format = "%d/%m/%Y")
+   x$fungicide_application_3 <- as.Date(x$fungicide_application_3, format = "%d/%m/%Y")
+   x$fungicide_application_4 <- as.Date(x$fungicide_application_4, format = "%d/%m/%Y")
+   x$fungicide_application_5 <- as.Date(x$fungicide_application_5, format = "%d/%m/%Y")
+   x$fungicide_application_6 <- as.Date(x$fungicide_application_6, format = "%d/%m/%Y")
+   x$fungicide_application_7 <- as.Date(x$fungicide_application_7, format = "%d/%m/%Y")
+   x$harvest_date <- as.Date(x$harvest_date, format = "%d/%m/%Y")
+   x$final_assessment <- as.Date(x$final_assessment, format = "%d/%m/%Y")
+   return(x)
 }
