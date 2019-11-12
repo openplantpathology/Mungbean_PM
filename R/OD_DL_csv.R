d515e9564b6876af878aefe8bd2109f4e0f56569
Adam H. Sparks <adamhsparks@gmail.com>
2019-11-12 12:36:57 +1000

    Fix bug that prevented this function from working on *nix systems
    Use `file.path()` in place of `paste0()` and remove the `\\` in the file path. If this breaks a Windows system, then we can implement a workaround to check for the OS before importing from `tempdir()`

--- R/OD_DL_csv.R
+++ R/OD_DL_csv.R
@@ -1,22 +1,17 @@
-OD_DL_csv <- function(sharedURL, file_name, save2wd = FALSE){
+OD_DL_csv <- function(sharedURL, file_name, save2wd = FALSE) {
    # Save the shared url
-   URL1 <- unlist(strsplit(sharedURL,"[?]"))[1]
-   URL1 <- paste0(URL1,"?download=1")
+   URL1 <- unlist(strsplit(sharedURL, "[?]"))[1]
+   URL1 <- paste0(URL1, "?download=1")
    
-   curl::curl_download(
-      URL1,
-      destfile = file.path(tempdir(), file_name),
-      mode = "wb"
-   )
+   curl::curl_download(URL1,
+                       destfile = file.path(tempdir(), file_name),
+                       mode = "wb")
    
-   if(isTRUE(save2wd)){
-      file.copy(
-         from = paste0(tempdir(),"\\", file_name),
-         to = "./")
+   if (isTRUE(save2wd)) {
+      file.copy(from = paste0(tempdir(), "\\", file_name),
+                to = "./")
    }
    
-   
-   
-   return(read.csv(paste0(tempdir(), "\\" ,file_name), stringsAsFactors = FALSE ))
-   
+   return(read.csv(file.path(tempdir(), file_name),
+                   stringsAsFactors = FALSE))
 }
