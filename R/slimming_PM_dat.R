c2979e8798ae92e09bcb476966f5e392ba891a47
PaulMelloy <paul@melloy.com.au>
2020-02-11 12:14:49 +1000

    did something here

--- R/slimming_PM_dat.R
+++ R/slimming_PM_dat.R
@@ -2,33 +2,33 @@
 # The slimed down dataset will be used for the Meta-analysis
 
 #source("R/import_data.R")
-# PM_MB_means <- import_data()
-PM_MB_means <- read.csv("data/1911_PM_MB_means&Ygains.csv")
+# PM_MB_Mtemp <- import_data()
+PM_MB_Mtemp <- read.csv("data/1911_PM_MB_means&Ygains.csv")
 
 slim_PM_dat <- data.frame(
-   trial = paste(PM_MB_means$trial_ref, PM_MB_means$year,
-                 PM_MB_means$location,PM_MB_means$host_genotype,
-                 PM_MB_means$row_spacing, sep = "_"),
-   trial_ref = as.character(PM_MB_means$trial_ref),
-   year = as.integer(PM_MB_means$year),
-   location = as.character(PM_MB_means$location),
-   row_spacing = as.double(PM_MB_means$row_spacing),
-   n = as.integer(PM_MB_means$replicates),
-   first_sign_disease = as.Date(PM_MB_means$first_sign_disease, format = "%Y-%m-%d"),
-   fungicide_ai = as.character(PM_MB_means$fungicide_ai),
-   fungicide_application_1 = as.Date(PM_MB_means$fungicide_application_1, format = "%Y-%m-%d"),
-   fungicide_application_2 = as.Date(PM_MB_means$fungicide_application_2, format = "%Y-%m-%d"),
-   fungicide_application_3 = as.Date(PM_MB_means$fungicide_application_3, format = "%Y-%m-%d"),
-   total_fungicide = as.integer(PM_MB_means$total_fungicide),
-   grain_yield.t.ha = as.double(PM_MB_means$grain_yield.t.ha.),
-   yield_error = as.double(PM_MB_means$yield_error),
-   yield_gain = as.double(PM_MB_means$yield_gain),
-   prop_yield_gain = as.double(PM_MB_means$prop_YG),
-   PM_final_severity = as.double(PM_MB_means$PM_final_severity),
-   PM_final_severity_error = as.double(PM_MB_means$disease_error),
-   Y_Msquare = as.double(PM_MB_means$Y_Msquare),
-   AUDPC_m = as.double(PM_MB_means$AUDPC_m),
-   AUDPC_se = as.double(PM_MB_means$AUDPC_m)
+   trial = paste(PM_MB_Mtemp$trial_ref, PM_MB_Mtemp$year,
+                 PM_MB_Mtemp$location,PM_MB_Mtemp$host_genotype,
+                 PM_MB_Mtemp$row_spacing, sep = "_"),
+   trial_ref = as.character(PM_MB_Mtemp$trial_ref),
+   year = as.integer(PM_MB_Mtemp$year),
+   location = as.character(PM_MB_Mtemp$location),
+   row_spacing = as.double(PM_MB_Mtemp$row_spacing),
+   n = as.integer(PM_MB_Mtemp$replicates),
+   first_sign_disease = as.Date(PM_MB_Mtemp$first_sign_disease, format = "%Y-%m-%d"),
+   fungicide_ai = as.character(PM_MB_Mtemp$fungicide_ai),
+   fungicide_application_1 = as.Date(PM_MB_Mtemp$fungicide_application_1, format = "%Y-%m-%d"),
+   fungicide_application_2 = as.Date(PM_MB_Mtemp$fungicide_application_2, format = "%Y-%m-%d"),
+   fungicide_application_3 = as.Date(PM_MB_Mtemp$fungicide_application_3, format = "%Y-%m-%d"),
+   total_fungicide = as.integer(PM_MB_Mtemp$total_fungicide),
+   grain_yield.t.ha = as.double(PM_MB_Mtemp$grain_yield.t.ha.),
+   yield_error = as.double(PM_MB_Mtemp$yield_error),
+   yield_gain = as.double(PM_MB_Mtemp$yield_gain),
+   prop_yield_gain = as.double(PM_MB_Mtemp$prop_YG),
+   PM_final_severity = as.double(PM_MB_Mtemp$PM_final_severity),
+   PM_final_severity_error = as.double(PM_MB_Mtemp$disease_error),
+   Y_Msquare = as.double(PM_MB_Mtemp$Y_Msquare),
+   AUDPC_m = as.double(PM_MB_Mtemp$AUDPC_m),
+   AUDPC_se = as.double(PM_MB_Mtemp$AUDPC_m)
 )
 
 
@@ -55,4 +55,3 @@ slim_PM_dat <- slim_PM_dat[slim_PM_dat$fungicide_ai == "control" |
 
 
 write.csv(slim_PM_dat, file = "data/slim_PM_dat.csv", row.names = FALSE)
-
