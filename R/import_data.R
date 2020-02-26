77c1a57e1f39ed8861f62e874c3a9c57181cc183
PaulMelloy <paul@melloy.com.au>
2020-02-26 11:15:24 +1000

    Some formatting done

--- R/import_data.R
+++ R/import_data.R
@@ -27,32 +27,50 @@ import_data <- function() {
    x$year <- as.factor(x$year)
    x$replicates <- as.numeric(x$replicates)
    x$planting_date <- as.Date(x$planting_date, format = "%Y-%m-%d")
-   x$emergence_date <- as.Date(x$emergence_date, format = "%Y-%m-%d")
-   x$flowering_date <- as.Date(x$flowering_date, format = "%Y-%m-%d")
+   x$emergence_date <-
+      as.Date(x$emergence_date, format = "%Y-%m-%d")
+   x$flowering_date <-
+      as.Date(x$flowering_date, format = "%Y-%m-%d")
    x$pod_fill_date <- as.Date(x$pod_fill_date, format = "%Y-%m-%d")
-   x$mid_late_pod_fill <- as.Date(x$mid_late_pod_fill, format = "%Y-%m-%d")
-   x$first_sign_disease <- as.Date(x$first_sign_disease, format = "%Y-%m-%d")
-   x$fungicide_application_1 <- as.Date(x$fungicide_application_1, format = "%Y-%m-%d")
-   x$fungicide_application_2 <- as.Date(x$fungicide_application_2, format = "%Y-%m-%d")
-   x$fungicide_application_3 <- as.Date(x$fungicide_application_3, format = "%Y-%m-%d")
-   x$fungicide_application_4 <- as.Date(x$fungicide_application_4, format = "%Y-%m-%d")
-   x$fungicide_application_5 <- as.Date(x$fungicide_application_5, format = "%Y-%m-%d")
-   x$fungicide_application_6 <- as.Date(x$fungicide_application_6, format = "%Y-%m-%d")
-   x$fungicide_application_7 <- as.Date(x$fungicide_application_7, format = "%Y-%m-%d")
+   x$mid_late_pod_fill <-
+      as.Date(x$mid_late_pod_fill, format = "%Y-%m-%d")
+   x$first_sign_disease <-
+      as.Date(x$first_sign_disease, format = "%Y-%m-%d")
+   x$fungicide_application_1 <-
+      as.Date(x$fungicide_application_1, format = "%Y-%m-%d")
+   x$fungicide_application_2 <-
+      as.Date(x$fungicide_application_2, format = "%Y-%m-%d")
+   x$fungicide_application_3 <-
+      as.Date(x$fungicide_application_3, format = "%Y-%m-%d")
+   x$fungicide_application_4 <-
+      as.Date(x$fungicide_application_4, format = "%Y-%m-%d")
+   x$fungicide_application_5 <-
+      as.Date(x$fungicide_application_5, format = "%Y-%m-%d")
+   x$fungicide_application_6 <-
+      as.Date(x$fungicide_application_6, format = "%Y-%m-%d")
+   x$fungicide_application_7 <-
+      as.Date(x$fungicide_application_7, format = "%Y-%m-%d")
    x$harvest_date <- as.Date(x$harvest_date, format = "%Y-%m-%d")
-   x$final_assessment <- as.Date(x$final_assessment, format = "%Y-%m-%d")
+   x$final_assessment <-
+      as.Date(x$final_assessment, format = "%Y-%m-%d")
    x$Y_error_type <- as.character(x$Y_error_type)
    
    #delete when base data is corrected
    # x[x$trial_ref == "mung1516/01", "harvest_date"] <- as.Date("2016-05-06", format = "%Y-%m-%d")
    # x[x$trial_ref == "mung1516/01", "final_assessment"] <- as.Date("2016-04-15", format = "%Y-%m-%d")
-
-   if(!c("AUDPS_m") %in% colnames(x)){x$AUDPS_m <- NA}
-   if(!c("AUDPS_sd") %in% colnames(x)){x$AUDPS_sd <- NA}
-   if(!c("Inc_Ms") %in% colnames(x)){x$Inc_Ms <- NA}
    
-      
-   x <- x[,!(colnames(x) == "X")]
+   if (!c("AUDPS_m") %in% colnames(x)) {
+      x$AUDPS_m <- NA
+   }
+   if (!c("AUDPS_sd") %in% colnames(x)) {
+      x$AUDPS_sd <- NA
+   }
+   if (!c("Inc_Ms") %in% colnames(x)) {
+      x$Inc_Ms <- NA
+   }
+   
+   
+   x <- x[, !(colnames(x) == "X")]
    
    return(x)
 }
