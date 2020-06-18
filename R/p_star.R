c189bbb1b7f13060400a668c7a6c247acda873ec
PaulMelloy <paul@melloy.com.au>
2020-06-19 07:59:19 +1000

    Function for easily visualise pvals

---
+++ R/p_star.R
@@ -0,0 +1,11 @@
+p_star <- function(x){
+   if(x > 0.08)return("")
+   if(x <= 0.08 &
+      x > 0.05) return(".")
+   if(x <= 0.05 &
+      x > 0.01) return("*")
+   if(x <= 0.01 &
+      x > 0.001) return("**")
+   if(x < 0.001) return("***")
+   
+}
