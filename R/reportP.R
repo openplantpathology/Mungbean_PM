ae015c25dc0d6e9417b7eb9ef4cc96aeac88ff04
PaulMelloy <paul@melloy.com.au>
2020-07-24 15:54:00 +1000

    Give a as.numeric option for reportP function

--- R/reportP.R
+++ R/reportP.R
@@ -1,7 +1,15 @@
-reportP <- function(p){
-   if(p < 0.0001){
+reportP <- function(p, AsNumeric = FALSE){
+   if(AsNumeric == FALSE){
+      if(p < 0.0001){
       return("P < 0.0001")
    }else{
       return(paste("P =", format(round(p, digits =  4), scientific = FALSE)))
-   }
+   }}
+   
+   if(AsNumeric == TRUE){
+      if(p < 0.0001){
+      return(format(0.0001, scientific = FALSE))
+   }else{
+      return(format(round(p, digits =  4), scientific = FALSE))
+   }}
 }
