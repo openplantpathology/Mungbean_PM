fb643754f5ada2676d4bbcc67e4da5d28e3df7c7
PaulMelloy <paul@melloy.com.au>
2020-07-31 21:25:01 +1000

    Add functionallity to function to allow for vectors of pvals

--- R/reportP.R
+++ R/reportP.R
@@ -1,4 +1,6 @@
 reportP <- function(p, AsNumeric = FALSE){
+   if(length(p) == 1){
+   
    if(AsNumeric == FALSE){
       if(p < 0.0001){
       return("P < 0.0001")
@@ -11,5 +13,24 @@ reportP <- function(p, AsNumeric = FALSE){
       return(format(0.0001, scientific = FALSE))
    }else{
       return(format(round(p, digits =  4), scientific = FALSE))
-   }}
+   }}}else{
+      
+      sapply(p,function(p) {
+         if(AsNumeric == FALSE){
+            if(p < 0.0001){
+               return("P < 0.0001")
+            }else{
+               return(paste("P =", format(round(p, digits =  4), scientific = FALSE)))
+            }}
+         
+         if(AsNumeric == TRUE){
+            if(p < 0.0001){
+               return(format(0.0001, scientific = FALSE))
+            }else{
+               return(format(round(p, digits =  4), scientific = FALSE))
+            }}
+      })
+      
+   }
+   
 }
