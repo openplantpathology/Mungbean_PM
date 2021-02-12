1dbf27678463c087d2eab4c7c06868cc26a06d6b
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-12 12:30:03 +1000

    add parameter to suppress "P" in character formatting

--- R/reportP.R
+++ R/reportP.R
@@ -1,34 +1,71 @@
-reportP <- function(p, AsNumeric = FALSE){
-   if(length(p) == 1){
+reportP <- function(p,
+                    AsNumeric = FALSE,
+                    P_prefix = TRUE) {
+   if (AsNumeric == TRUE &
+       P_prefix == TRUE) {
+      warning("'AsNumeric' and 'P_prefix' can't both be 'TRUE'.\n
+           Forcing `P_prefix = FALSE`")
+      P_prefix <- FALSE
+      # normally I would make this a stop()
+      # but I want to make the lastest version of this function backwards compatible
+   }
    
-   if(AsNumeric == FALSE){
-      if(p < 0.0001){
-      return("P < 0.0001")
-   }else{
-      return(paste("P =", format(round(p, digits =  4), scientific = FALSE)))
-   }}
-   
-   if(AsNumeric == TRUE){
-      if(p < 0.0001){
-      return(format(0.0001, scientific = FALSE))
-   }else{
-      return(format(round(p, digits =  4), scientific = FALSE))
-   }}}else{
+   if (length(p) == 1) {
+      if (AsNumeric == FALSE &
+          P_prefix == TRUE) {
+         if (p < 0.0001) {
+            return("P < 0.0001")
+         } else{
+            return(paste("P =", format(
+               round(p, digits =  4), scientific = FALSE
+            )))
+         }
+      }
+      if (AsNumeric == FALSE &
+          P_prefix == FALSE) {
+         if (p < 0.0001) {
+            return("< 0.0001")
+         } else{
+            return(format(round(p, digits =  4), scientific = FALSE))
+         }
+      }
       
-      sapply(p,function(p) {
-         if(AsNumeric == FALSE){
-            if(p < 0.0001){
+      if (AsNumeric == TRUE) {
+         if (p < 0.0001) {
+            return(format(0.0001, scientific = FALSE))
+         } else{
+            return(format(round(p, digits =  4), scientific = FALSE))
+         }
+      }
+   } else{
+      sapply(p, function(p) {
+         if (AsNumeric == FALSE &
+             P_prefix == TRUE) {
+            if (p < 0.0001) {
                return("P < 0.0001")
-            }else{
-               return(paste("P =", format(round(p, digits =  4), scientific = FALSE)))
-            }}
+            } else{
+               return(paste("P =", format(
+                  round(p, digits =  4), scientific = FALSE
+               )))
+            }
+         }
          
-         if(AsNumeric == TRUE){
-            if(p < 0.0001){
-               return(format(0.0001, scientific = FALSE))
-            }else{
+         if (AsNumeric == FALSE &
+             P_prefix == FALSE) {
+            if (p < 0.0001) {
+               return("< 0.0001")
+            } else{
                return(format(round(p, digits =  4), scientific = FALSE))
-            }}
+            }
+         }
+         
+         if (AsNumeric == TRUE) {
+            if (p < 0.0001) {
+               return(format(0.0001, scientific = FALSE))
+            } else{
+               return(format(round(p, digits =  4), scientific = FALSE))
+            }
+         }
       })
       
    }
